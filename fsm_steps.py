75ac052434008d53aa6e0cd3d1ba442257a46027
olvikt <39388293+olvikt@users.noreply.github.com>
2025-04-17 07:36:04 +0300

    Update fsm_steps.py

--- fsm_steps.py
+++ fsm_steps.py
@@ -1,1 +1,15 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
+class Form(StatesGroup):
+    choose_language = State()
+    full_name = State()
+    job_title = State()
+    expertise = State()
+    help_offer = State()
+    upload_photo = State()
+    contact_phone = State()
+    social_links = State()
+    about_business = State()
+    website_link = State()
+    custom_link = State()
+    done = State()
